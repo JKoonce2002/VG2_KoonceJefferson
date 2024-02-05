f462a4d77812858420b984dd0e3a15554ea70f6a
JKoonce2002 <71308122+JKoonce2002@users.noreply.github.com>
2024-02-05 17:13:38 -0600

    Q2 Complete

---
+++ VG2_KoonceJefferson/Assets/Scripts/KillZone.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Q2
+{
+    public class KillZone : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void OnTriggerEnter(Collider other)
+        {
+            if(other.GetComponent<Collider>())
+            {
+                string currentScene = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene(currentScene);
+            }
+        }
+    }
+}
